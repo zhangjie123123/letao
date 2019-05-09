a15496b2401912673bc2879367e339f56e5ce84c
zhangjie123123 <1846175615@qq.com>
2019-05-09 11:35:47 +0800

    ajax登录效验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -2,7 +2,13 @@
  * Created by Administrator on 2019-05-09.
  */
 $(function () {//入口函数
-$('#form').bootstrapValidator({
+    $('#form').bootstrapValidator({
+    //配置效验的图标
+    feedbackIcons:{
+        valid:'glyphicon glyphicon-ok',
+        invalid:'glyphicon glyphicon-remove',
+        validating:'glyphicon glyphicon-refresh'
+    },
 // 配置字段
     fields: {
         username: {
@@ -16,6 +22,9 @@ $('#form').bootstrapValidator({
                     min: 2,
                     max: 6,
                     message: "用户名在2-6位"
+                },
+                callback:{
+                    message:"用户名不存在"
                 }
             }
         },
@@ -29,6 +38,9 @@ $('#form').bootstrapValidator({
                     min: 6,
                     max: 12,
                     message: "密码长度必须是6-12位"
+                },
+                callback:{
+                    message:"密码错误"
                 }
 
             }
@@ -36,4 +48,33 @@ $('#form').bootstrapValidator({
         }
     }
 });//调插件方法//一般传对象
+    //功能2 登录功能
+    // 表单效验成功事件
+    $('#form').on('success.form.bv',function (e) {
+        //阻止默认的表单提交
+        e.preventDefault();
+        //通过ajax进行提交
+        $.ajax({
+            type:"post",
+            url:"/employee/employeeLogin",
+            data:$('#form').serialize(),//获取form里面的值
+            dataType:"json",
+            success:function (info) {
+                console.log(info);
+                if(info.success){
+                    location.href ="index.html";
+                }
+                if(info.error===1000){
+                    $('#form').data("bootstrapValidator").updateStatus("username","INVALID","callback");
+                }
+                if(info.error===1001){
+                    $('#form').data("bootstrapValidator").updateStatus("password","INVALID","callback");
+                }
+            }
+        })
+    });
+    //3.重置功能
+    $('[type="reset"]').click(function () {
+        $('#form').data("bootstrapValidator").resetForm(true);
+    })
 });
