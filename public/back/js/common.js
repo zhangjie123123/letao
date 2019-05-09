94fd323c6e2617c01f29ec5ab7bd604a7c63ed03
zhangjie123123 <1846175615@qq.com>
2019-05-09 12:13:04 +0800

    添加进度条功能

---
+++ public/back/js/common.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2019-05-09.
+ */
+
+
+$(document).ajaxStart(function () {
+//1.开启进度条
+    NProgress.start();
+});
+$(document).ajaxStop(function () {
+//1.开启进度条
+    setTimeout(function () {
+        NProgress.done();
+    },500);
+
+});
