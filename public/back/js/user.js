4577f2e4e715b13f0b5c748cd11dd2f77d7394ac
zhangjie123123 <1846175615@qq.com>
2019-05-10 10:16:34 +0800

    用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -4,6 +4,8 @@
 $(function () {
     var currentPage = 2;
     var pageSize = 5;
+    var currentId;
+    var isDelete;
  //1.一进入页面，发送ajax请求获取用户列表数据，通过模板引擎渲染
     render();
     function render() {
@@ -37,6 +39,33 @@ $(function () {
         });
     };
 
-    //分页初始化
-
+//2.点击启用 禁用启动模态框，通过事件委托绑定
+    $('tbody').on('click','.btn',function () {
+        //显示模态框
+        $('#userModal').modal("show");
+        //获取用户id
+        currentId= $(this).parent().data('id');
+        isDelete=$(this).hasClass('btn-danger')?0:1;
+    });
+//3.点击确认按钮,修改对应用户状态
+    $('#submitBtn').click(function () {
+       //
+        $.ajax({
+            type:'post',
+            url:'/user/updateUser',
+            data:{
+                id:currentId,
+                isDelete:isDelete
+            },
+            dataType:'json',
+            success:function (info) {
+               //1.关闭模态框
+                if(info.success){
+                    $('#userModal').modal('hide');
+                }
+                //2.页面要重新渲染
+                render();
+            }
+        });
+    });
 });
