860faaf979132ce258ddad906f43fead16ac8e0f
zhangjie123123 <1846175615@qq.com>
2019-05-10 09:14:42 +0800

    用户管理分页渲染完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -2,19 +2,41 @@
  * Created by Administrator on 2019-05-10.
  */
 $(function () {
+    var currentPage = 2;
+    var pageSize = 5;
  //1.一进入页面，发送ajax请求获取用户列表数据，通过模板引擎渲染
-    $.ajax({
-        type:"get",
-        url:"/user/queryUser",
-        data:{
-            page:1,
-            pageSize:5
-        },
-        dataType:"json",
-        success:function (info) {
-            // console.log(info);
-            var htmlStr=template('tpl',info);
-            $('tbody').html(htmlStr);
-        }
-    })
+    render();
+    function render() {
+        $.ajax({
+            type:"get",
+            url:"/user/queryUser",
+            data:{
+                page:currentPage,
+                pageSize:pageSize
+            },
+            dataType:"json",
+            success:function (info) {
+                // console.log(info);
+                var htmlStr=template('tpl',info);
+                $('tbody').html(htmlStr);
+
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion:3,
+                    totalPages:Math.ceil(info.total/info.size),
+                    //当前显示第几页
+                    currentPage:info.page,
+                //    当页码被点击时调用的回调函数
+                    onPageClicked:function (a,b,c,page) {
+                        // console.log(page);
+                        currentPage=page;
+                        render();
+                    }
+
+                });
+            }
+        });
+    };
+
+    //分页初始化
+
 });
